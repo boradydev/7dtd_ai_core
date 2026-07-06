38d9178fa09d452d92cdcc469c4aa47d0706aa60
boradydev <alexvlod85@gmail.com>
2026-07-06 10:31:11 +0300

    test: add integration test for `LocalizationGameData` in `GameDataUOW` with `pytest-timeout`

--- tests/integration/infrastructure/db/postgres/conftest.py
+++ tests/integration/infrastructure/db/postgres/conftest.py
@@ -9,13 +9,7 @@ from src.infrastructure.db.postgres.settings import PostgresSettings
 
 @pytest.fixture
 def postgres_settings() -> PostgresSettings:
-    return PostgresSettings(
-        POSTGRES_HOST="localhost",
-        POSTGRES_PORT="5432",
-        POSTGRES_USER="postgres",
-        POSTGRES_PASSWORD="postgres",
-        POSTGRES_DB="test_7dtd",
-    )
+    return PostgresSettings()
 
 
 @pytest.fixture
---
+++ tests/integration/infrastructure/db/postgres/uow/game_data/test_game_data_localization_uow.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.application.game_data.schemas.localization import LocalizationGameData
+from src.infrastructure.csv_parser.common.parser import UniversalCsvParser
+from src.infrastructure.csv_parser.patterns.localization import PATTERN
+
+
+@pytest.mark.timeout(5)
+async def test_game_data_localization_repo(
+    game_data_uow,
+    config_dir,
+) -> None:
+    file_path = config_dir / "localization.csv"
+
+    parser = UniversalCsvParser(
+        file_path=file_path,
+        pattern=PATTERN,
+    )
+
+    game_data = [LocalizationGameData.model_validate(row) for row in parser.run()]
+
+    async with game_data_uow as uow:
+        await uow.localization.clear()
+        await uow.localization.add_many(game_data)
+        await uow.commit()
